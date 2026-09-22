72446f2286f1e81acf27bbf1febafdde4bce6b43
Adnan10p <Adnan10p@users.noreply.github.com>
2026-09-22 10:24:51 +0500

    Clean repository: Remove unused BUILDER B, remix, and scratch directories

--- scratch/test_download_endpoint.ts
+++
@@ -1,8 +0,0 @@
-import http from "http";
-
-http.get("http://localhost:3000/DroidForge-Companion-v5.0.0-Master.apk", (res) => {
-  console.log("Status Code:", res.statusCode);
-  console.log("Content-Type:", res.headers["content-type"]);
-  console.log("Content-Disposition:", res.headers["content-disposition"]);
-  console.log("Content-Length:", res.headers["content-length"]);
-});
